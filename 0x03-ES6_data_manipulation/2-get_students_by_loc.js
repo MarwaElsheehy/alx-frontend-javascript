02053425cd966688bdad4f0cac04779489728aba
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2024-06-03 19:23:51 +0300

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(lst, city) {
-  return lst.filter((obj) => obj.location === city);
+export default function getStudentsByLocation(arr, city) {
+  return arr.filter((student) => student.location === city);
 }
