07c5221a1774d74c8a332f0cae6a748c3d87d08f
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2024-06-03 19:21:00 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(arr) {
     if (!Array.isArray(arr)) {
         return [];
-    } else {
-        return arr.map((e) => e.id);
     }
+        return arr.map((e) => e.id);
 }
