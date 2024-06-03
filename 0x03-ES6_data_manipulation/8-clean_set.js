55f451e7e57f85ff48c55001a7d950c8a9ae1832
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2024-06-03 19:54:43 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,11 @@
 export default function cleanSet(set, startString) {
-  let text = '';
-  const array = [];
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
 
-  if (startString && typeof startString === 'string') {
-    for (const element of set) {
-      if (element && element.startsWith(startString)) {
-        array.push(element.slice(startString.length));
-      }
-    }
-    text = array.join('-');
-  }
-  return text;
+  let finalString = '';
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) finalString += `${element.slice(startString.length)}-`;
+  });
+
+  return finalString.slice(0, finalString.length - 1);
 }
-
 
