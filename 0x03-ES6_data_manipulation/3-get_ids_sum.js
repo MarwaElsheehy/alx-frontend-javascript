cd3a7d71a6b97500dd7dff318df72496107f9dc2
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2024-06-03 19:26:13 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(arr) {
-  return arr.reduce((counter, currentObj) => counter.id + currentObj.id, 0);
+  return arr.reduce((counter, currentObj) => counter + currentObj, 0);
 }
