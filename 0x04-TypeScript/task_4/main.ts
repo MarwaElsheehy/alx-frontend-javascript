1f020c54d7c869b7f0c48992e0ef33c43e43a4a5
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2024-06-06 00:32:04 +0300

    Update main.ts

--- 0x04-TypeScript/task_4/main.ts
+++ 0x04-TypeScript/task_4/main.ts
@@ -1,9 +1,3 @@
-/// <reference path="subjects/Teacher.ts" />
-/// <reference path="subjects/Subject.ts" />
-/// <reference path="subjects/Cpp.ts" />
-/// <reference path="subjects/React.ts" />
-/// <reference path="subjects/Java.ts" />
-
 export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
 export const react = new Subjects.React();
